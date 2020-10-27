bcc1788d77e27521e816c7e38c4c96334696d833
Marco A Marquez <marco.marquez.b@gmail.com>
2020-10-27 17:05:46 +0100

    adding mocking plugin

--- bin/add_mocking_plugin.sh
+++ bin/add_mocking_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LOCAL_PLUGIN="$HOME/Projects/kong-plugin-mocking/kong/plugins/mocking"
+LOCAL_PLUGIN="/Users/marco/Code/kong-plugin-mocking/kong/plugins/mocking"
 
 docker cp  $LOCAL_PLUGIN kong-ent1:/usr/local/share/lua/5.1/kong/plugins
 docker exec -ti kong-ent1 /bin/sh -c "KONG_PLUGINS='bundled,mocking' kong reload"
