3880765014f6c27098ed95ba910eaf96e6857ceb
Marco A Marquez <marco.marquez.b@gmail.com>
2020-11-10 09:32:21 +0100

    new version for delete_document_objects

--- bin/delete_document_objects.sh
+++ bin/delete_document_objects.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # The goal of the script is to remove the "Document Objects" entities in Kong Enterprise that cannot be removed by decK due to the following issue: https://github.com/Kong/deck/issues/208
 
-http GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' | while read id; do
+http GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq --raw-output -c '.data[].id' | while read id; do
     echo "Deleting the document_object with the ID: ${id}"
     http --ignore-stdin DELETE http://localhost:8001/document_objects/$id Kong-Admin-Token:KongRul3z! 
 done
