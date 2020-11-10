d56b5afd4860da15c06cd65131fc4b129d64ea65
Marco A Marquez <marco.marquez.b@gmail.com>
2020-11-10 09:02:05 +0100

    update

--- .github/actions/portal-deploy-action/docker_deploy.sh
+++ .github/actions/portal-deploy-action/docker_deploy.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
 pushd /tmp
-
-http $ADMIN_HOST:$ADMIN_PORT/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' | while read id; do
-    echo "Deleting the document_object with the ID: ${id}"
-    http --ignore-stdin DELETE http://localhost:8001/document_objects/$id Kong-Admin-Token:KongRul3z! 
-done
-
 deck sync
