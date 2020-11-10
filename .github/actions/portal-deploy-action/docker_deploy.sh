d7929cf2bfe311605f602ec64a1014a7c92457fe
Marco A Marquez <marco.marquez.b@gmail.com>
2020-11-10 09:34:45 +0100

    final update?

--- .github/actions/portal-deploy-action/docker_deploy.sh
+++ .github/actions/portal-deploy-action/docker_deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
 pushd /tmp
-
-echo "[before deck] Number of Document Objects: "
-http http://localhost:8001/document_objects | jq -r .data | jq length
-
 echo "Running deck sync..."
 deck sync
-
-echo "[after deck] Number of Document Objects: "
-http http://localhost:8001/document_objects | jq -r .data | jq length
