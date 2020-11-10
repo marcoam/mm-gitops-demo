62c3ae58b197c4c1e834f74c999f9df5335e9114
Marco A Marquez <marco.marquez.b@gmail.com>
2020-11-10 09:10:38 +0100

    update to sh

--- bin/delete_document_objects.sh
+++ bin/delete_document_objects.sh
@@ -4,8 +4,23 @@
 echo "Checking HTTPie version: "
 http --version
 
+echo "http --verbose http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! ...."
+http --verbose http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z!
+echo "-------"
+echo "http --verbose http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' ...."
+http --verbose http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id'
+echo "-------"
+echo "-------"
+echo "http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! ...."
+http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z!
+echo "-------"
+echo "http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' ...."
+http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id'
 
+
+
+echo "Retrieving all of the document objects in Kong..."
 http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' | while read id; do
     echo "Deleting the document_object with the ID: ${id}"
-    http --ignore-stdin DELETE $ADMIN_HOST:$ADMIN_PORT/document_objects/$id Kong-Admin-Token:KongRul3z! 
+    http --ignore-stdin DELETE http://localhost:8001/document_objects/$id Kong-Admin-Token:KongRul3z! 
 done
