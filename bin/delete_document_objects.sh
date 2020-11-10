b73105788e10e3e9cfb85077f38520441ae915d1
Marco A Marquez <marco.marquez.b@gmail.com>
2020-11-10 09:14:22 +0100

    update to sh

--- bin/delete_document_objects.sh
+++ bin/delete_document_objects.sh
@@ -4,23 +4,23 @@
 echo "Checking HTTPie version: "
 http --version
 
-echo "http --verbose http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! ...."
-http --verbose http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z!
+echo "#1 - http --verbose GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! ...."
+http --verbose GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z!
 echo "-------"
-echo "http --verbose http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' ...."
-http --verbose http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id'
+echo "#2 - http --verbose GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' ...."
+http --verbose GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id'
 echo "-------"
 echo "-------"
-echo "http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! ...."
-http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z!
+echo "#3 - http GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! ...."
+http GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z!
 echo "-------"
-echo "http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' ...."
-http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id'
+echo "#4 - http GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' ...."
+http GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id'
 
 
 
 echo "Retrieving all of the document objects in Kong..."
-http http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' | while read id; do
+http GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq -c '.data[].id' | while read id; do
     echo "Deleting the document_object with the ID: ${id}"
     http --ignore-stdin DELETE http://localhost:8001/document_objects/$id Kong-Admin-Token:KongRul3z! 
 done
