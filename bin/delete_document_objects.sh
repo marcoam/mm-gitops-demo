60573cf210f06086b109aa0071f6958665563cd5
Marco Marquez <marco.marquez.b@gmail.com>
2022-02-04 10:14:52 +0100

    Pushing updates to fix Application Deletion and Clean up Names

--- bin/delete_document_objects.sh
+++ bin/delete_document_objects.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# The goal of the script is to remove the "Document Objects" entities in Kong Enterprise that cannot be removed by decK due to the following issue: https://github.com/Kong/deck/issues/208
+# The goal of the script is to remove the "Document Objects" and "Applications" entities in Kong Enterprise that cannot be removed by decK due to the following issue: https://github.com/Kong/deck/issues/208
 
 echo "Number of Document Objects to delete: "
 http http://localhost:8001/document_objects | jq -r .data | jq length
@@ -8,4 +8,14 @@ echo "Starting to delete the document_objects..."
 http GET http://localhost:8001/document_objects Kong-Admin-Token:KongRul3z! | jq --raw-output -c '.data[].id' | while read id; do
     echo "Deleting the document_object with the ID: ${id}"
     http --ignore-stdin DELETE http://localhost:8001/document_objects/$id Kong-Admin-Token:KongRul3z! 
+done
+
+
+echo "Number of Applications to delete: "
+http http://localhost:8001/applications | jq -r .data | jq length
+
+echo "Starting to delete the Applications..."
+http GET http://localhost:8001/applications Kong-Admin-Token:KongRul3z! | jq --raw-output -c '.data[].id' | while read id; do
+    echo "Deleting the document_object with the ID: ${id}"
+    http --ignore-stdin DELETE http://localhost:8001/applications/$id Kong-Admin-Token:KongRul3z! 
 done
