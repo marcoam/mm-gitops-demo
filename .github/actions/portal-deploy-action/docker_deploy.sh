9624a7a2f37d5217813ca091ebe77c54acb35333
Marco Marquez <marco.marquez.b@gmail.com>
2022-10-12 12:06:30 +0200

    Pushing to deck latest version

--- .github/actions/portal-deploy-action/docker_deploy.sh
+++ .github/actions/portal-deploy-action/docker_deploy.sh
@@ -2,6 +2,6 @@
 
 pushd /tmp
 echo "Running deck convert..."
-deck convert --from kong-gateway-2.x --to kong-gateway-3.x --input-file=/tmp/kong.yaml --output-file kong-converted.yaml --verbose=2
+deck convert --from kong-gateway-2.x --to kong-gateway-3.x --input-file=/tmp/kong.yaml --output-file kong-converted.yaml
 echo "Running deck sync..."
-deck sync -s kong-converted.yaml --verbose=2
+deck sync -s kong-converted.yaml
