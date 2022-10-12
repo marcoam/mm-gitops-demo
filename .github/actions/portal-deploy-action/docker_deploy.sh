c621646d7a669c9a836a37202de5376a160ebec4
Marco Marquez <marco.marquez.b@gmail.com>
2022-10-12 11:53:50 +0200

    update to deck 3.0

--- .github/actions/portal-deploy-action/docker_deploy.sh
+++ .github/actions/portal-deploy-action/docker_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 pushd /tmp
+echo "Running deck convert..."
+deck convert --from kong-gateway-2.x --to kong-gateway-3.x --input-file=/tmp/kong.yaml --output-file kong-converted.yaml --verbose=2
 echo "Running deck sync..."
-deck convert --from kong-gateway-2.x --to kong-gateway-3.x --verbose=2
-deck sync --verbose=2
+deck sync -s kong-converted.yaml --verbose=2
