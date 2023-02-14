d7c3a95b2723c18810fce47235529283bdf687eb
Marco Marquez <marco.marquez.b@gmail.com>
2023-02-14 11:28:54 +0100

    Adding Service and Routes

--- bin/add_mocking_plugin.sh
+++ bin/add_mocking_plugin.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-LOCAL_PLUGIN="/Users/marco/Code/kong-plugin-mocking/kong/plugins/mocking"
+#LOCAL_PLUGIN="/Users/marco/Code/kong-plugin-mocking/kong/plugins/mocking"
+LOCAL_PLUGIN="/Users/marcomarquez/Code/kong-plugin-mocking/kong/plugins/mocking"
 
 docker cp  $LOCAL_PLUGIN kong-ent1:/usr/local/share/lua/5.1/kong/plugins
 docker exec -ti kong-ent1 /bin/sh -c "KONG_PLUGINS='bundled,mocking' kong reload"
